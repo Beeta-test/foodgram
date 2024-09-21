b9cb8fee7021406d2880efbdc23cc74c4156fb02
Maxim Cherdansev <HeikouSeen@yandex.ru>
2024-09-21 23:58:11 +0300

    fodgram

--- backend/core/consts.py
+++ backend/core/consts.py
@@ -4,4 +4,4 @@ BASE_UTIL_LEGHT: int = 50
 USERNAME_LENGTH: int = 150
 USER_LENGTH: int = 150
 EMAIL_LENGTH: int = 254
-BASE_URl: str = 'https://foodgram.example.org/s/'
+BASE_URl: str = 'https://foodandmemes.serveminecraft.net/s/'
